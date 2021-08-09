4de60770869f0596a36752284a2e4af6fea248d8
Natalie Z <78460783+memermaid@users.noreply.github.com>
2021-08-09 13:53:08 -0700

    Update to NimmGame

--- NimmGame/nimmgame.py
+++ NimmGame/nimmgame.py
@@ -1,34 +1,34 @@
 def main():
     stones = 20
+    turn = 1
 
-    while stones >= 3:
-        p1 = int(input('Player 1 would you like to remove 1 or 2 stones? '))
-        stones -= p1
-        print(f"There are {stones} stones left.")
-        p2 = int(input('Player 2 would you like to remove 1 or 2 stones? '))
-        stones -= p2
-        print(f"There are {stones} stones left.")
-
-    p1 = int(input('Player 1 would you like to remove 1 or 2 stones? '))
-    stones -= p1
-
-    if stones == 0:
-        print("Player 2 won!")
-    elif stones < 0:
-        print("You can't take more stones that there are available.")
-        p1 = int(input('Player 1 would you like to remove 1 or 2 stones? '))
-        stones -= p1
+    while stones > 0:
+        if turn == 1:
+            print('Player 1')
+            amount = askforinput()
+            stones -= amount
+            turn += 1
+            print(f"There are {stones} stones left.\n")
+        elif turn == 2:
+            print("Player 2")
+            amount = askforinput()
+            stones -= amount
+            print(f"There are {stones} stones left.\n")
+            turn -= 1
+    if turn == 1:
+        print("Player 1 won!")
     else:
-        p2 = int(input('Player 2 would you like to remove 1 or 2 stones? '))
-        stones -= p2
-        if stones == 0:
-            print("Player 2 won!")
-        elif stones < 0:
-            print("You can't take more stones that there are available.")
+        print("Player 2 won!")        
 
-
-
-
+def askforinput():
+    while True:
+        try:
+            msg = int(input('Would you like to remove 1 or 2 stones? '))
+            if msg == 1 or msg == 2:
+                break
+        except ValueError:
+            print("Invalid input.")
+    return msg
 
 
 
